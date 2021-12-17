4dbe7d8e7c0ad1264c5104de432c71862a9812e6
ankit127 <ankitkothawade14@gmail.com>
2021-12-17 17:38:14 +0530

    My Project

---
+++ src/app/Interface/student-obj.ts
@@ -0,0 +1,9 @@
+export class StudentObj {
+
+    studentRollNo?: number;
+    name?: string ;
+    grade?: number ;
+    division?: string;
+    dateofBirth?: string;
+    gender?: string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentHomeComponent } from './StudentComponents/student-home/student-home.component';
+import { EditComponent } from './StudentComponents/edit/edit.component';
+import { ExtraActivityComponent } from './StudentComponents/extra-activity/extra-activity.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentHomeComponent,
+    EditComponent,
+    ExtraActivityComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
